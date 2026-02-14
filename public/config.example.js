0357c03d8c18482fc89f8c7db484420eae5c5d80
Mirza Bal <mirzabursa@hotmail.com>
2026-02-15 01:17:15 +0300

    Create config.example.js

---
+++ public/config.example.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "YOUR_FIREBASE_API_KEY",
+    authDomain: "YOUR_FIREBASE_DOMAIN",
+    projectId: "YOUR_FIREBASE_PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID"
+};
